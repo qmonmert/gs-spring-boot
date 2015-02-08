8d7d6c0cd74490713f026cf6b69cc62ffb4a83f4
Quentin Monmert <q.monmert@proxiad.com>
2015-02-08 18:05:30 +0100

    Adapt the app to run in Tomcat 7

---
+++ src/main/java/DispatcherConfig.java
@@ -0,0 +1,23 @@
+
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+//@Configuration
+public class DispatcherConfig { //extends WebMvcConfigurerAdapter {
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+//        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+//        registry.addResourceHandler("/html/**").addResourceLocations("/html/");
+//        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+//        registry.addResourceHandler("/**").addResourceLocations("/");
+//        registry.addResourceHandler("/views/**").addResourceLocations("/views/");
+//    }
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//    }
+
+}
