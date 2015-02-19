9e34e79ce3bcb8769a6b3e2a2e2df21ba3da0ecb
Quentin Monmert <q.monmert@proxiad.com>
2015-02-19 17:27:48 +0100

    Update the application.yml and ApplicationWebXml.java for Openshift

--- src/main/java/ApplicationWebXml.java
+++ src/main/java/ApplicationWebXml.java
@@ -1,20 +1,20 @@
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * This is an helper Java class that provides an alternative to creating a web.xml.
  */
-public class ApplicationWebXml {// extends SpringBootServletInitializer {
+public class ApplicationWebXml extends SpringBootServletInitializer {
 
-//    private final Logger log = LoggerFactory.getLogger(ApplicationWebXml.class);
-//
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application
-//                .showBanner(false)
-//                .sources(Application.class);
-//    }
+    private final Logger log = LoggerFactory.getLogger(ApplicationWebXml.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application
+                .showBanner(false)
+                .sources(Application.class);
+    }
 
 }
